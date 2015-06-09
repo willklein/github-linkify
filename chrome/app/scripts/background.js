9a0ce75ef7632112c84699a3c1b0b80da4b648e4
Will Klein <will.klein@gmail.com>
2015-06-08 21:08:34 -0600

    Clicking the page action from repo pages will navigate to the repo

--- chrome/app/scripts/background.js
+++ chrome/app/scripts/background.js
@@ -21,13 +21,18 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
+  var repo;
   var url = tab.url;
   var parts = url.split('/');
   var domain = parts[2];
   var domainParts = domain.split('.');
   var subdomain = domainParts[0];
 
+  if (parts.length > 3) {
+    repo = parts[3];
+  }
+
   chrome.tabs.update(tab.id, {
-    url: 'https://github.com/' + subdomain
+    url: 'https://github.com/' + subdomain + (repo ? '/' + repo : '')
   });
 });
